22c2fd88a3120150e0c0105172d46bb24bc43f70
Sebas121616 <seb.caicedo16@gmail.com>
2025-12-12 11:54:09 -0500

    Nuevo login

--- login.py
+++ login.py
@@ -1,8 +1,10 @@
-datos = input("Ingrese su nombre de usuario")
-Usuario = "Sebas"
-if datos == Usuario:
-    print ("Su usuario es correcto")
-else:
-    print("Su usuario es incorrecto")
-
-print ("Version 3")
+Usuario = "2"
+datos = "1"
+while Usuario != datos:
+    datos = input("Ingrese su nombre de usuario: ")
+    Usuario = "Sebas"
+    if datos == Usuario:
+        print ("Su usuario es correcto")
+    else:
+        Usuario = input("Ingrese su nuevo usuario: ")
+        print("Su usuario es incorrecto")
